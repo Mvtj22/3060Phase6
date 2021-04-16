a8b984c09728d858ebdad73c2488caa654b39544
Mvtj22 <mexsupermario@gmail.com>
2021-04-16 14:05:43 -0400

    Changes

--- Phase6Daily.sh
+++ Phase6Daily.sh
@@ -4,11 +4,15 @@ echo "Create Fresh version of project"
 export WORKING_DIR=$PWD
 export PROJECT_DIR="Project"
 export BACK_END="BackEnd"
-if [ -d "$WORKING_DIR" ]; then rm -Rf $PROJECT_DIR; fi
-if [ -d "$WORKING_DIR" ]; then rm -Rf $BACK_END; fi
+#if [ -d "$WORKING_DIR" ]; then rm -Rf $PROJECT_DIR; fi
+#if [ -d "$WORKING_DIR" ]; then rm -Rf $BACK_END; fi
 
-git clone https://github.com/Mvtj22/3060FrontEnd.git "$PROJECT_DIR"
-git clone https://github.com/jonathan-hermans/SQA "$BACK_END"
+#git clone https://github.com/Mvtj22/3060FrontEnd.git "$PROJECT_DIR"
+#git clone https://github.com/jonathan-hermans/SQA "$BACK_END"
+
+echo "Building Current Project"
+cd "$WORKING_DIR/$PROJECT_DIR/testFile/testFile"
+make 
 
 export BUILD="../$PROJECT_DIR/testFile/testFile/"
 echo "Build Path: $BUILD"
@@ -22,8 +26,11 @@ cd $BUILD
 
 # Iterate over our input files and run it. 
 for i in $ITEMS
-do 
-  export INPUT_NAME=$(echo $i | sed -r "s/.+\/(.+)\..+/\1/")
-  echo "Executing Input: $INPUT_NAME"
-  cp "./ioFiles/session.etf" "../../../Outputs/"
+do
+  if [[ ${i: -4} == ".inp" ]]; then 
+    export INPUT_NAME=$(echo $i | sed -r "s/.+\/(.+)\..+/\1/")
+    echo "Executing Input: $INPUT_NAME"
+    cat "../../../Inputs/$i" |"./BankingSystem" > "../../../Outputs/Output1.bto"
+    cp "./ioFiles/session.etf" "../../../Outputs/"
+  fi
 done
